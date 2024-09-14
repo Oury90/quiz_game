07809c7e29a5d7d8d7d059eebaab2bf83081cfa5
DIALLO <112278900+Oury90@users.noreply.github.com>
2024-09-13 23:24:19 -0500

    added a data file

---
+++ datas/data.py
@@ -0,0 +1,9 @@
+question_data = [
+    {"text": "A slug's blood is green.", "answer": "True"},
+    {"text": "A slug's blood is green.", "answer": "False"},
+    {"text": "A slug's blood is green.", "answer": "True"},
+    {"text": "A slug's blood is green.", "answer": "True"},
+    {"text": "A slug's blood is green.", "answer": "False"},
+    {"text": "A slug's blood is green.", "answer": "True"}
+]
+
